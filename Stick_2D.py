0c0ae5d887d74c3d50401fbb7fb456b3d7b47a36
wyf-75038 <84698909+wyf-75038@users.noreply.github.com>
2021-06-15 16:38:41 +0800

    Add files via upload

--- Stick_2D.py
+++ Stick_2D.py
@@ -1,7 +1,7 @@
 import numpy as np
+from scipy import linalg
 import matplotlib.pyplot as plt
 from matplotlib import cm
-from scipy import linalg
 
 
 def input_2d(file_name):  # 输入
@@ -148,89 +148,131 @@ def sigma_solve():  # 求解应力
 
 
 def txt_output_2d(txt_name, dig):
+    def add_seq(matrix):
+        line = matrix.shape[0]
+        seq = np.array([i for i in range(1, line + 1)])
+        seq = seq.reshape(line, 1)
+        seq_add_matrix = np.block([[seq, matrix]])
+        return seq_add_matrix
+
+    u_output = add_seq(UMatrix * 1000).round(dig)
+    f_output = add_seq(FMatrix / 1000).round(dig)
+
+    sigma_line = Sigma.shape[0]
+    sigma_t = Sigma.reshape(sigma_line, 1)
+
+    sigma_output = add_seq(sigma_t / 10 ** 6).round(dig)
+
     with open(txt_name, 'w') as FOutput:
-        FOutput.write('2D Bracing System Results\n')
+        FOutput.write('2D Truss System Results\n')
         FOutput.write('U/mm\n')
-        FOutput.write(str((UMatrix * 1000).round(dig)))
+        FOutput.write(str(u_output))
         FOutput.write('\n')
         FOutput.write('F/kN\n')
-        FOutput.write(str((FMatrix / 1000).round(dig)))
+        FOutput.write(str(f_output))
         FOutput.write('\n')
         FOutput.write('sigma/MPa\n')
-        FOutput.write(str((Sigma / 10 ** 6).round(dig)))
+        FOutput.write(str(sigma_output))
         FOutput.write('\n')
-    return 1
 
 
 def fig_output_2d(fig_name, u_factor):
-    node_x = Node[:, 0]
-    node_y = Node[:, 1]
-    for i in range(StickNum):
-        x = [Node[Stick[i, 0] - 1, 0], Node[Stick[i, 1] - 1, 0]]
-        y = [Node[Stick[i, 0] - 1, 1], Node[Stick[i, 1] - 1, 1]]
-        plt.plot(x, y, color='gray', linestyle=':', dash_capstyle='round', linewidth=2, zorder=0)
+    def stick_draw():  # 绘制杆件原始位置
+        for i in range(StickNum):
+            x = [Node[Stick[i, 0] - 1, 0], Node[Stick[i, 1] - 1, 0]]
+            y = [Node[Stick[i, 0] - 1, 1], Node[Stick[i, 1] - 1, 1]]
+            plt.plot(x, y, color='gray', linestyle=':', dash_capstyle='round', linewidth=2, zorder=0)
+
+    def node_draw(circle_size):  # 绘制节点原始位置
+        node_x = Node[:, 0]
+        node_y = Node[:, 1]
+        plt.scatter(node_x, node_y, circle_size, facecolors='w', edgecolors='gray', zorder=1)
+
+    def value_max(matrix, num):  # 求最大值
+        value = np.zeros(num)
+        for i in range(num):
+            value[i] = (matrix[i, 0] ** 2 + matrix[i, 1] ** 2) ** 0.5
+        value_max = np.max(value)
+        return value_max
+
+    def f_draw(f_factor):  # 绘制外载荷
+        for i in ForceNode:
+            plt.quiver(Node[i - 1, 0] - Force[i - 1, 0] / f_factor, Node[i - 1, 1] - Force[i - 1, 1] / f_factor,
+                       Force[i - 1, 0] / f_factor, Force[i - 1, 1] / f_factor, angles='xy', scale_units='xy', scale=1,
+                       color='gray', zorder=2)
+
+    def stick_result_draw(node_result, cmap, cmap_par):  # 绘制杆件计算结果
+        for i in range(StickNum):
+            x = [node_result[Stick[i, 0] - 1, 0], node_result[Stick[i, 1] - 1, 0]]
+            y = [node_result[Stick[i, 0] - 1, 1], node_result[Stick[i, 1] - 1, 1]]
+            plt.plot(x, y, c=cmap(cmap_par[i]), linewidth=3, zorder=3)
+        cb = plt.colorbar()
+        cb.set_label('$\it{\sigma}$(normalized)')
+
+    def normalize(var, num):  # 归一化
+        var_normalized = np.zeros(num)
+        for i in range(num):
+            var_normalized[i] = (var[i] - np.min(var)) / (np.max(var) - np.min(var))
+        return var_normalized
+
+    def node_result_draw(node_result):  # 绘制节点计算结果
+        node_result_x = node_result[:, 0]
+        node_result_y = node_result[:, 1]
+        u_value = np.zeros(NodeNum)
+        for i in range(NodeNum):
+            u_value[i] = 1000 * (UMatrix[i, 0] ** 2 + UMatrix[i, 1] ** 2) ** 0.5
+        plt.scatter(node_result_x, node_result_y, circle_size, c=u_value, cmap='jet', edgecolors='k', zorder=4)
+
     circle_size = 60
-    plt.scatter(node_x, node_y, circle_size, facecolors='w', edgecolors='gray', zorder=1)
+    stick_draw()
+    node_draw(circle_size)
+    node_distance_max = value_max(Node, NodeNum)
+    f_value_max = value_max(Force, NodeNum)
 
-    # 求节点与原点距离最大值
-    node2origin_distance = np.zeros(NodeNum)
-    for i in range(NodeNum):
-        node2origin_distance[i] = (Node[i, 0] ** 2 + Node[i, 1] ** 2) ** 0.5
-    distance_node2origin_max = np.max(node2origin_distance)
+    f_factor = 5 * f_value_max / node_distance_max  # 确定外载荷缩小系数
 
-    # 求外载荷最大值
-    f_value = np.zeros(NodeNum)
-    for i in range(NodeNum):
-        f_value[i] = (Force[i, 0] ** 2 + Force[i, 1] ** 2) ** 0.5
-    f_value_max = np.max(f_value)
-
-    f_factor = 5 * f_value_max / distance_node2origin_max  # 确定外载荷缩小系数
-
-    # 绘制外载荷
-    for i in ForceNode:
-        plt.quiver(Node[i - 1, 0] - Force[i - 1, 0] / f_factor, Node[i - 1, 1] - Force[i - 1, 1] / f_factor,
-                   Force[i - 1, 0] / f_factor, Force[i - 1, 1] / f_factor, angles='xy', scale_units='xy', scale=1,
-                   color='gray', zorder=2)
+    f_draw(f_factor)
 
     node_result = Node + u_factor * UMatrix
-    node_result_x = node_result[:, 0]
-    node_result_y = node_result[:, 1]
 
     sigma_abs = abs(Sigma)
-    sigma_normalized = np.zeros(StickNum)  # 归一化
-    for i in range(StickNum):
-        sigma_normalized[i] = (sigma_abs[i] - np.min(sigma_abs)) / (np.max(sigma_abs) - np.min(sigma_abs))
+    sigma_normalized = normalize(sigma_abs, StickNum)
     cmap_stick = cm.get_cmap('viridis')
+    stick_result_draw(node_result, cmap_stick, sigma_normalized)
 
-    for i in range(StickNum):
-        x = [node_result[Stick[i, 0] - 1, 0], node_result[Stick[i, 1] - 1, 0]]
-        y = [node_result[Stick[i, 0] - 1, 1], node_result[Stick[i, 1] - 1, 1]]
-        plt.plot(x, y, c=cmap_stick(sigma_normalized[i]), linewidth=3, zorder=3)
+    node_result_draw(node_result)
     cb = plt.colorbar()
-    cb.set_label('sigma(normalized)')
-
-    u_value = np.zeros(NodeNum)
-    for i in range(NodeNum):
-        u_value[i] = 1000 * (UMatrix[i, 0] ** 2 + UMatrix[i, 1] ** 2) ** 0.5
-    plt.scatter(node_result_x, node_result_y, circle_size, c=u_value, cmap='jet', edgecolors='k', zorder=4)
-
-    cb = plt.colorbar()
-    cb.set_label('u/mm')
+    cb.set_label('$\it{u}$/mm')
     plt.axis('equal')
+    fig_title = 'Visualization Result, Amplification Factor of u = ' + str(u_factor)
+    plt.title(fig_title)
     plt.savefig(fig_name, dpi=600)
     plt.show()
-    return 1
 
 
-DeBug = 1
-if DeBug:
-    FileNameInput = 'DataInput_2D_Ex1.txt'
-    Dig = 3
-    Fac = 50
-else:
-    FileNameInput = input('请输入输入文件名\n')
-    Dig = int(input('请输入计算结果保留小数位数\n'))
-    Fac = float(input('请输入位移放大系数\n'))
+def whether_default(var, default, var_type):
+    if len(var.strip(' ')) == 0 or var.lower() == 'default':  # “default”不区分大小写
+        var = var_type(default)
+    else:
+        var = var_type(var)
+    return var
+
+
+DigDefault = 3
+FacDefault = 50
+
+FileNameInput = input('请输入输入文件名：\n')
+print('以下内容不输入或输入空格或输入“default”，则使用默认值。')
+print('请输入计算结果保留小数位数，默认值为', DigDefault, '：')
+Dig = input()
+print('请输入位移放大系数，默认值为', FacDefault, '：')
+Fac = input()
+
+if FileNameInput[-4:] != '.txt':  # 如果最后4个字符不是“.txt”
+    FileNameInput = FileNameInput + '.txt'  # 防止用户忘记输入后缀名
+
+Dig = whether_default(Dig, DigDefault, int)
+Fac = whether_default(Fac, FacDefault, float)
 
 TxtNameOutput = 'Result_of_' + FileNameInput
 FigNameOutput = 'Result_of_' + FileNameInput.strip('.txt') + '.png'
